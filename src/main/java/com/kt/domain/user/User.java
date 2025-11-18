0db6ca30a9ab184afc5b8f28d22eddcee1fe2009
DaveLee-b <lpoln1855@gmail.com>
2025-11-18 17:32:19 +0900

    fix: delete id from User (#4)

--- src/main/java/com/kt/domain/user/User.java
+++ src/main/java/com/kt/domain/user/User.java
@@ -16,10 +16,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @Column(nullable = false, length = 30)
     private String loginId;
 
