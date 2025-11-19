aea310d2bae4c839eb9424fd26b4f47736b481ad
JONGHAN <howeer15325@naver.com>
2025-11-19 12:09:45 +0900

    feat: review 등록 개발

--- src/main/java/com/kt/dto/review/ReviewCreateReqeust.java
+++ src/main/java/com/kt/dto/review/ReviewCreateReqeust.java
@@ -1,28 +1,21 @@
 package com.kt.dto.review;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-/**
- *packageName    : com.kt.dto.review
- * fileName       : ReviewCreateReqeust
- * author         : howee
- * date           : 2025-11-18
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2025-11-18        howee       최초 생성
- */
 public record ReviewCreateReqeust(
 
-	@NotBlank
+	@NotBlank(message = "제목은 필수입니다")
 	String title,
 
-	@NotBlank
+	@NotBlank(message = "상세 내용은 필수입니다")
 	String description,
 
-	@NotNull
+	@NotNull(message = "별점은 필수입니다")
+	@Min(value = 1, message = "별점은 1 이상이어야 합니다")
+	@Max(value = 5, message = "별점은 5 이하여야 합니다")
 	Integer star
 ) {
 }
