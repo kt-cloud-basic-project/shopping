22eb662abffd0fb81e36d7f596db8c001c8b5e54
JONGHAN <howeer15325@naver.com>
2025-11-19 23:31:05 +0900

    feat: review 사용자 CRUD 개발 / createRequest 삭제

--- src/main/java/com/kt/service/review/ReviewService.java
+++ src/main/java/com/kt/service/review/ReviewService.java
@@ -1,11 +1,17 @@
 package com.kt.service.review;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kt.common.ErrorCode;
+import com.kt.common.Preconditions;
 import com.kt.domain.review.Review;
-import com.kt.dto.review.ReviewCreateReqeust;
+import com.kt.dto.review.ReviewReqeust;
+import com.kt.dto.review.ReviewResponse;
+import com.kt.repository.product.ProductRepository;
 import com.kt.repository.review.ReviewRepository;
 import com.kt.repository.user.UserRepository;
 
@@ -18,12 +24,15 @@ public class ReviewService {
 
 	private final ReviewRepository reviewRepository;
 	private final UserRepository userRepository;
+	private final ProductRepository productRepository;
+	//private final OrderProductRepository orderProductRepository;
 
-	public void create(Long productId, ReviewCreateReqeust reqeust) {
+	public void create(Long productId, ReviewReqeust.Create reqeust) {
 		var user = userRepository.findByIdOrThrow(2L, ErrorCode.NOT_FOUND_USER);
 
-		//TODO 프로덕트 검증
-		//productId
+		//var orderProduct = productRepository.findById(productId);
+
+		//Preconditions.validate(product.isPresent(), ErrorCode.NOT_FOUND_PRODUCT);
 
 		Review review = new Review(
 			user,
@@ -35,4 +44,43 @@ public class ReviewService {
 		reviewRepository.save(review);
 
 	}
+
+	public Page<ReviewResponse.ReviewList> myReviewList(Long userId, Pageable pageable) {
+		var user = userRepository.findByIdOrThrow(userId, ErrorCode.NOT_FOUND_USER);
+
+		Page<Review> reviewList = reviewRepository.findReviewsByUserId(user.getId(), pageable);
+
+		return ReviewResponse.ReviewList.fromList(reviewList);
+	}
+
+	public void update(Long reviewId, ReviewReqeust.Update request) {
+		var user = userRepository.findByIdOrThrow(2L, ErrorCode.NOT_FOUND_USER);
+
+		var review = reviewRepository.findByIdOrThrow(reviewId, ErrorCode.NOT_FOUND_REVIEW);
+
+		review.update(
+			user,
+			request.description(),
+			request.star()
+		);
+	}
+
+	public void delete(Long reviewId) {
+		var user = userRepository.findByIdOrThrow(2L, ErrorCode.NOT_FOUND_USER);
+
+		var review = reviewRepository.findByIdOrThrow(reviewId, ErrorCode.NOT_FOUND_REVIEW);
+
+		review.delete(user);
+
+	}
+
+	/*public Page<ReviewResponse.ReviewList> productReviewList(Long productId, Pageable pageable) {
+		//TODO: findByIdOrThrow 추가되면 수정
+		var product = productRepository.findById(productId);
+		Preconditions.validate(product.isPresent(), ErrorCode.NOT_FOUND_PRODUCT);
+
+		Page<Review> reviewList = reviewRepository.findReviewsByProductId(productId, pageable);
+
+		return ReviewResponse.ReviewList.fromList(reviewList);
+	}*/
 }
