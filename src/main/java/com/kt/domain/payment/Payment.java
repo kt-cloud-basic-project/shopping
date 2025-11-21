21f0e2db952e3fb68707cca4fe5bf288b70d36cf
Jungyo Yang(Window) <2jungyo@naver.com>
2025-11-22 00:56:14 +0900

    SHOP-8 feat: payment domain 개발

--- src/main/java/com/kt/domain/payment/Payment.java
+++ src/main/java/com/kt/domain/payment/Payment.java
@@ -1,4 +1,56 @@
 package com.kt.domain.payment;
 
-public class Payment {
+import com.kt.common.BaseEntity;
+// import com.kt.domain.order.Order;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Payment extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Integer totalPrice;
+
+	@Column(nullable = false)
+	private Integer deliveryFee;
+
+	@Column(nullable = false)
+	private Integer finalPrice;
+
+	@Column(nullable = false)
+	private boolean isDeleted = false;
+
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "order_id", nullable = false)
+	// private Order order;
+
+	// @Enumerated(EnumType.STRING)
+	// @Column(name = "payment_type_id", nullable = false)
+	// private PaymentType paymentType;
+
+	public Payment(
+		//Order order,
+		Integer totalPrice,
+		Integer deliveryFee,
+		Integer finalPrice
+		//PaymentType paymentType
+	) {
+		// this.order = order;
+		this.totalPrice = totalPrice;
+		this.deliveryFee = deliveryFee;
+		this.finalPrice = finalPrice;
+		// this.paymentType = paymentType;
+		this.isDeleted = false;
+	}
+
+	public void delete() {
+		this.isDeleted = true;
+	}
 }
