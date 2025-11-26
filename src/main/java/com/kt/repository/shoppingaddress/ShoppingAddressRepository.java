c676592caf4c1053f52cf1ea9c0a2842d7e26060
DaveLee-b <lpoln1855@gmail.com>
2025-11-26 17:19:16 +0900

    refactor: 정보조회시 주소지도 반환하도록 수정

--- src/main/java/com/kt/repository/shoppingaddress/ShoppingAddressRepository.java
+++ src/main/java/com/kt/repository/shoppingaddress/ShoppingAddressRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kt.domain.shoppingaddress.ShoppingAddress;
 
+import java.util.Optional;
+
 public interface ShoppingAddressRepository extends JpaRepository<ShoppingAddress, Long> {
+    Optional<ShoppingAddress> findFirstByUserIdAndIsDefaultTrueOrderByIdDesc(Long userId);
 }
