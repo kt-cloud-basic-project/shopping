69bc0204d8dbdc0f7dc99824cdd9713b426fea3a
전지민 <jimin@jeonjimin-ui-MacBookPro.local>
2025-11-28 00:48:47 +0900

    feat: order 목록 조회 개발

--- src/main/java/com/kt/controller/order/OrderController.java
+++ src/main/java/com/kt/controller/order/OrderController.java
@@ -1,19 +1,28 @@
 package com.kt.controller.order;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kt.common.request.Paging;
 import com.kt.common.response.ApiResult;
 import com.kt.dto.order.OrderCreateRequest;
+import com.kt.dto.order.response.OrderListResponse;
 import com.kt.security.CustomUserDetails;
 import com.kt.service.order.OrderService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Order", description = "주문 관련 API")
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
@@ -21,9 +30,25 @@ public class OrderController {
 	private final OrderService orderService;
 
 	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	@Operation(summary = "주문 생성")
 	public ApiResult<Void> create(@AuthenticationPrincipal CustomUserDetails currentUser,
 		@Valid @RequestBody OrderCreateRequest orderCreateRequest) {
 		orderService.create(currentUser.getId(), orderCreateRequest);
 		return ApiResult.ok();
 	}
+
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "주문 목록 조회")
+	public ApiResult<Page<OrderListResponse>> getOrderList(
+		Paging paging,
+		@AuthenticationPrincipal CustomUserDetails currentUser
+		) {
+		Page<OrderListResponse> orderList = orderService.getOrderList(currentUser.getId(), paging);
+
+		return ApiResult.ok(orderList);
+	}
+
+
 }
