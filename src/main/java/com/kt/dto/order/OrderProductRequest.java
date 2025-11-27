0747505e6a653d5bb434ebc3766cc9287638e624
munyeong0103 <skymy02@ajou.ac.kr>
2025-11-27 15:42:31 +0900

    fix: order-create-dto 유효값 설정 변경 (#50)

--- src/main/java/com/kt/dto/order/OrderProductRequest.java
+++ src/main/java/com/kt/dto/order/OrderProductRequest.java
@@ -7,7 +7,7 @@ public record OrderProductRequest(
 	@NotNull(message = "상품 선택은 필수입니다")
 	Long productId,
 	@NotNull(message = "상품 수량 선택은 필수입니다")
-	@Min(value = 0, message = "상품 수량은 1개 이상 선택해야 합니다")
+	@Min(value = 1, message = "상품 수량은 1개 이상 선택해야 합니다")
 	Long productCount,
 	@NotNull(message = "상품 옵션 선택은 필수입니다")
 	Long productVariantId
