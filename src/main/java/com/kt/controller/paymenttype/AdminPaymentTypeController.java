52ca00030538d34c3b64c10917bb589c82669f36
Jungyo Yang(Window) <2jungyo@naver.com>
2025-11-29 00:16:34 +0900

    feat:관리자권한기능 추가o

--- src/main/java/com/kt/controller/paymenttype/AdminPaymentTypeController.java
+++ src/main/java/com/kt/controller/paymenttype/AdminPaymentTypeController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/payments/types")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminPaymentTypeController {
 
 	private final PaymentTypeService paymentTypeService;
