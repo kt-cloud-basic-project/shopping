a6d730a8a08a1e50248d54637f2d4991b726ee1b
Jungyo Yang(Window) <2jungyo@naver.com>
2025-11-30 20:26:16 +0900

    feat: 결제(Payment) 생성, controller제외

--- src/main/java/com/kt/controller/payment/PaymentController.java
+++ src/main/java/com/kt/controller/payment/PaymentController.java
@@ -1,7 +1,11 @@
 package com.kt.controller.payment;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kt.common.response.ApiResult;
 import com.kt.dto.payment.PaymentCreateRequest;
@@ -10,14 +14,13 @@ import com.kt.service.payment.PaymentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/payments")
 public class PaymentController {
 
-	private final PaymentService paymentService;
+	// private final PaymentService paymentService;
 
-	@PostMapping
-	public ApiResult<Void> create(PaymentCreateRequest request) {
-		paymentService.create(request);
-	}
+
 }
+
