5da3c37c568565a40cc57636d4e42c4605857aa6
DaveLee-b <lpoln1855@gmail.com>
2025-11-30 20:01:45 +0900

    fix: @PreAuthorize 개별적용

--- src/main/java/com/kt/controller/user/AdminUserController.java
+++ src/main/java/com/kt/controller/user/AdminUserController.java
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@PreAuthorize("hasRole('ADMIN')")
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/users")
@@ -26,12 +26,14 @@ public class AdminUserController {
         userService.createAdmin(request);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/user")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ApiResult<UserInfoResponse> userInfo(@Valid @RequestBody UserIdRequest request){
         UserInfoResponse userInfo = userService.getUserInfo(request.userId());
         return ApiResult.ok(userInfo);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ApiResult<Page<UserListResponse>> userList(Pageable pageable){
@@ -40,6 +42,7 @@ public class AdminUserController {
     }
 
     //유저 정보 수정
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/update")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ApiResult<Void> updateUserInfo(@Valid @RequestBody UserAdminUpdateRequest request){
@@ -47,6 +50,7 @@ public class AdminUserController {
         return ApiResult.ok();
     }
     //유저 비밀번호 수정
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/update/password")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ApiResult<Void> changeUserPassword(@Valid @RequestBody UserAdminChangePassword request){
@@ -54,6 +58,7 @@ public class AdminUserController {
         return ApiResult.ok();
     }
     //권한변경
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/update/role")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ApiResult<Void> changeUserPassword(@Valid @RequestBody UserChangeRole request){
