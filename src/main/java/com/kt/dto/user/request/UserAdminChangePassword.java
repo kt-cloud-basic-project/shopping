e0161a9e0a26fc48c26935616a7da27f5b1cb3d6
DaveLee-b <lpoln1855@gmail.com>
2025-12-03 21:56:40 +0900

    feat: User 요청초기값 추가

--- src/main/java/com/kt/dto/user/request/UserAdminChangePassword.java
+++ src/main/java/com/kt/dto/user/request/UserAdminChangePassword.java
@@ -1,10 +1,14 @@
 package com.kt.dto.user.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record UserAdminChangePassword(
+        @Schema(description = "관리자 로그인 아이디", example = "testAdmin")
         @NotNull
         String userId,
+
+        @Schema(description = "관리자 로그인 비밀번호", example = "Test@1234")
         @NotNull
         String password
 ) {
--- src/main/java/com/kt/dto/user/request/UserChangePassword.java
+++ src/main/java/com/kt/dto/user/request/UserChangePassword.java
@@ -1,8 +1,10 @@
 package com.kt.dto.user.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record UserChangePassword(
+        @Schema(description = "수정할 비밀번호", example = "Test@12345")
         @NotNull
         String password
 ){
