452694d634d0becc8a1f2b6978e892455b6ea711
JONGHAN <howeer15325@naver.com>
2025-12-09 15:34:49 +0900

    feat: 운영 환경 분리 local, dev, prod

--- src/main/java/com/kt/common/profile/AppProfile.java
+++ src/main/java/com/kt/common/profile/AppProfile.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import org.springframework.context.annotation.Profile;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Profile({"local"})
-public @interface LocalProfile {
+@Profile("prod")
+public @interface AppProfile {
 }
