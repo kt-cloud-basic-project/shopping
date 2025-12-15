18b75de292cb52c2f789256d443f326dcc04de7a
munyeong0103 <skymy02@ajou.ac.kr>
2025-12-15 09:14:38 +0900

    test: product service-test setup (#118)

--- src/test/java/com/kt/service/CategoryServiceTest.java
+++ src/test/java/com/kt/service/CategoryServiceTest.java
@@ -1,4 +1,4 @@
-package com.kt.service.category;
+package com.kt.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +12,7 @@ import com.kt.dto.category.CategoryRequest;
 import com.kt.repository.category.CategoryRepository;
 import com.kt.repository.product.ProductRepository;
 import com.kt.repository.variant.VariantRepository;
+import com.kt.service.category.CategoryService;
 
 import static org.assertj.core.api.Assertions.*;
 
