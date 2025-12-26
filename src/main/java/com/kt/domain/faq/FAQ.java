82b2e1af1adf2a3d285c58aad25d979be87c0284
Jungyo Yang(Window) <2jungyo@naver.com>
2025-12-27 02:06:23 +0900

    feat: add wishlist CRUD operations

---
+++ src/main/java/com/kt/domain/faq/FAQ.java
@@ -0,0 +1,11 @@
+package com.kt.domain.faq;
+
+import com.kt.common.support.BaseEntity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class FAQ extends BaseEntity {
+private String title;
+private 
+}
