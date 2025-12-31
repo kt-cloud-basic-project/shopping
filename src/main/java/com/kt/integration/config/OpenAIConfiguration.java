6c9c5e086be5c1e7b2ecfcbcf154f4b149af3b62
jimin <jeemin065@gmail.com>
2025-12-31 15:49:54 +0900

    fix: OpenAIConfiguration 테스트 환경에서 비활성화

--- src/main/java/com/kt/integration/config/OpenAIConfiguration.java
+++ src/main/java/com/kt/integration/config/OpenAIConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.api.BaseAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("!test")
 public class OpenAIConfiguration {
 	@Bean
 	public ChatClient chatClient(ChatClient.Builder builder, BaseAdvisor openAICustomAdvisor) {
